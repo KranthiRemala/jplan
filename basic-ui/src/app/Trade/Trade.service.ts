d2c26f0af4ec86707bca3bc2e569dcd6c8a64250
Kranthi Remala <Kranthi.Remala@jda.com>
2019-04-22 18:38:48 +0530

    Initial draft of the final working versions.

---
+++ basic-ui/src/app/Trade/Trade.service.ts
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Injectable } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs/Observable';
+import { Trade } from '../jda.test.tutorialNetwork';
+import 'rxjs/Rx';
+import {HttpClient} from '@angular/common/http';
+
+// Can be injected into a constructor
+@Injectable()
+export class TradeService {
+
+  private NAMESPACE = 'Trade';
+  private ledgerUrl = "http://localhost:3000/api/Trade";
+
+  constructor(private dataService: DataService<Trade>, private client: HttpClient) {
+  };
+
+  public getAll(): Observable<Trade[]> {
+      return this.dataService.getAll(this.NAMESPACE);
+  }
+
+  public getTransaction(id: any): Observable<Trade> {
+    return this.dataService.getSingle(this.NAMESPACE, id);
+  }
+
+  public addTransaction(itemToAdd: any): Observable<Trade> {
+    return this.dataService.add(this.NAMESPACE, itemToAdd);
+  }
+
+  public updateTransaction(id: any, itemToUpdate: any): Observable<Trade> {
+    return this.dataService.update(this.NAMESPACE, id, itemToUpdate);
+  }
+
+  public deleteTransaction(id: any): Observable<Trade> {
+    return this.dataService.delete(this.NAMESPACE, id);
+  }
+
+  public getLedger() {
+    return this.client.get(this.ledgerUrl);
+  } 
+
+}
+
